895ac3040763866ef714187e060f6d774f5839d5
Kester Arenas <klfarenas@gmail.com>
2013-12-09 16:42:14 +0800

    created file chooser

---
+++ src/FileChooser.java
@@ -0,0 +1,86 @@
+import java.awt.BorderLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+public class FileChooser extends JPanel implements ActionListener {
+	JButton openButton;
+	JTextArea log;
+
+	FileChooser() {
+		super(new BorderLayout());
+
+		// Create the log first, because the action listeners
+		// need to refer to it.
+		log = new JTextArea(5, 20);
+		log.setMargin(new Insets(5, 5, 5, 5));
+		log.setEditable(false);
+		JScrollPane logScrollPane = new JScrollPane(log);
+
+		// Create the open button. We use the image from the JLF
+		// Graphics Repository (but we extracted it from the jar).
+		openButton = new JButton("Open a File...");
+		openButton.addActionListener(this);
+
+		// For layout purposes, put the buttons in a separate panel
+		JPanel buttonPanel = new JPanel();
+		buttonPanel.add(openButton);
+
+		// Add the buttons and the log to this panel.
+		add(buttonPanel, BorderLayout.PAGE_START);
+		add(logScrollPane, BorderLayout.CENTER);
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		// Handle open button action.
+		if (e.getSource() == openButton) {
+			JFileChooser chooser = new JFileChooser();
+			FileNameExtensionFilter filter = new FileNameExtensionFilter(
+					"FASTA & CSV", "fa", "csv");
+			chooser.setFileFilter(filter);
+			int returnVal = chooser.showOpenDialog(this);
+			if (returnVal == JFileChooser.APPROVE_OPTION) {
+				System.out.println("You chose to open this file: "
+						+ chooser.getSelectedFile().getAbsolutePath());
+				log.append("You chose to open this file: " + chooser.getSelectedFile().getAbsolutePath() + "\n");
+				log.setCaretPosition(log.getDocument().getLength());
+			}
+		}
+	}
+
+	private static void createAndShowGUI() {
+		// Create and set up the window.
+		JFrame frame = new JFrame("FileChooser");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		// Add content to the window.
+		frame.add(new FileChooser());
+
+		// Display the window.
+		frame.pack();
+		frame.setVisible(true);
+	}
+
+	public static void main(String args[]) {
+		//Schedule a job for the event dispatch thread:
+        //creating and showing this application's GUI.
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                //Turn off metal's use of bold fonts
+                UIManager.put("swing.boldMetal", Boolean.FALSE);
+                createAndShowGUI();
+            }
+        });
+
+	}
+}
